667ba39bec821c65cc4bed92004c5eeefeb7e8ed
Jun Oh <kyungjunjoh@gmail.com>
2019-01-07 14:54:50 -0800

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Jun Oh on 2019-01-07.
+//  Copyright © 2019 Jun Oh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
